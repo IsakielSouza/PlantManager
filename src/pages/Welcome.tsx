439b6b80a8eed0ca82c9636480600814f6aa1755
Isakiel Souza <isakielsouza@outlook.com.br>
2021-05-12 10:58:56 -0300

    Aula 01 - RN NLW05 - Ajustes layout tela de Welcome(CSS)

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { 
+import {
+  View, 
   Text,
   Image,
   SafeAreaView,
@@ -10,7 +11,8 @@ import {
 
 import { Feather } from '@expo/vector-icons';
 
-import colors from '../styles/colors'
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 import wateringImg from '../assets/watering.png';
 
@@ -18,53 +20,63 @@ export function Welcome() {
   return(
     <SafeAreaView style={styles.container}>
 
-      <Text style={styles.title}>
-        Gerencie{'\n'}
-        suas plantas de{'\n'}
-        forma fácil
-      </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie{'\n'}
+          suas plantas de{'\n'}
+          forma fácil
+        </Text>
 
-      <Image 
-        source={wateringImg}
-        style={styles.image}
-        resizeMode='contain'
-      ></Image>
+        <Image 
+          source={wateringImg}
+          style={styles.image}
+          resizeMode='contain'
+        ></Image>
 
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
 
-      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+        >
 
-      <Text>
-        <Feather 
-          name='chevron-right'
-          style={styles.buttonIcon}
-        />
-      </Text>
+          <Text>
+            <Feather 
+              name='chevron-right'
+              style={styles.buttonIcon}
+            />
+          </Text>
 
-      </TouchableOpacity>
+        </TouchableOpacity>
+      </View>  
 
     </SafeAreaView>
 
-    
+
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
-    marginTop: 20
+    paddingHorizontal: 20,    
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: 38,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   image: {
     height: Dimensions.get('window').width * 0.7
@@ -73,7 +85,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text,
   },
   button: {
     backgroundColor: colors.green,
